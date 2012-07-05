d4903c4b4c9eacdceb70fc787b24b2de7acc570a
cenkovic <cenkovic@propulsionapp.com>
2012-07-05 14:38:05 +0200

    dbSucc

--- db/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `category` (`id`, `title`, `content_type_id`, `user_id`, `gallery`, `files`, `system_category`) VALUES
-(26, 'default', 194, 6, 0, 0, 1),
-(27, 'default', 195, 6, 0, 0, 1),
-(31, 'default', 198, 6, 0, 0, 1);
