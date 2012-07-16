d084ab241a35198e486cc2815dbe64cbe9af6232
cenkovic <cenkovic@propulsionapp.com>
2012-07-16 11:15:09 +0200

    dbSucc

---
+++ dbpatches/2.sql
@@ -0,0 +1,4 @@
+INSERT INTO `category` (`id`, `title`, `content_type_id`, `user_id`, `gallery`, `files`, `system_category`) VALUES
+(null, 'Kategorija 1', 194, 6, 0, 0, 1),
+(null, 'Kategorija 2', 195, 6, 0, 0, 1),
+(null, 'Kategorija 22', 198, 6, 0, 0, 1);
