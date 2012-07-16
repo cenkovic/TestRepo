a718b06ece29822f4ce8be74ec5379c60167a356
cenkovic <cenkovic@propulsionapp.com>
2012-07-16 11:33:39 +0200

    dbSucc

--- dbpatches/2.sql
+++ dbpatches/2.sql
@@ -1,4 +1,4 @@
-INSERT INTO `category` (`id`, `title`, `content_type_id`, `user_id`, `gallery`, `files`, `system_category`) VALUES
-(null, 'Kategorija 1', 194, 6, 0, 0, 1),
-(null, 'Kategorija 2', 195, 6, 0, 0, 1),
-(null, 'Kategorija 22', 198, 6, 0, 0, 1);
+INSERT INTO `cars` (`id`, `title`, `color`, `num_of_doors`) VALUES
+(null, 'BMW', 'blue', 5),
+(null, 'Yugo', 'white', 3),
+(null, 'Renault', 'white', 3);
